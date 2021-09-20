43586116688223ddd926edb1cc8bfa4c00036a96
JosipMuzic-bebabit <josip.muzic@bebabit.com>
2021-09-20 18:15:13 +0200

    Add initial AppPadding class

---
+++ lib/utils/app_padding.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppPadding {
+  static const a24 = EdgeInsets.all(24);
+  static const a32 = EdgeInsets.all(32);
+
+  static const h32 = EdgeInsets.symmetric(horizontal: 32);
+}

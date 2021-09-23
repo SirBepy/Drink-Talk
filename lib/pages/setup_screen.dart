d01996de84cd9ae9edb0d3fa550ed1527229563a
JosipMuzic-bebabit <josip.muzic@bebabit.com>
2021-09-23 19:44:22 +0200

    Add Initial SetupScreen

--- lib/pages/setup_screen.dart
+++ lib/pages/setup_screen.dart
@@ -1,23 +1,136 @@
 import 'package:drink_n_talk/components/bottom_button.dart';
 import 'package:drink_n_talk/components/custom_app_bar.dart';
+import 'package:drink_n_talk/components/dotted_divider.dart';
+import 'package:drink_n_talk/components/loading_indicator.dart';
+import 'package:drink_n_talk/models/room.dart';
+import 'package:drink_n_talk/services/room_service.dart';
+import 'package:drink_n_talk/utils/app_padding.dart';
+import 'package:drink_n_talk/utils/spacers.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class SetupScreen extends StatelessWidget {
+class SetupScreen extends StatefulWidget {
   const SetupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SetupScreen> createState() => _SetupScreenState();
+}
+
+class _SetupScreenState extends State<SetupScreen> {
+  final RoomService roomService = RoomService();
+  Room? room;
+
+  @override
+  void initState() {
+    super.initState();
+    () async {
+      room = await roomService.createRoom();
+      setState(() {});
+    }();
+  }
+
+  Widget createPlayerListItem(String player, int index) {
+    return Text(
+      '$index. $player',
+      style: Theme.of(context).textTheme.headline1?.copyWith(color: Theme.of(context).primaryColor),
+    );
+  }
+
+  Widget createPlayersList(List<String> players) {
+    final List<Widget> children = [];
+
+    children.add(createPlayerListItem(players[0], 1));
+
+    for (int x = 1; x < players.length; x++) {
+      children.add(const Padding(padding: AppPadding.v16, child: DottedDivider()));
+      children.add(createPlayerListItem(players[x], x + 1));
+    }
+
+    return Padding(
+      padding: AppPadding.a48,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Prijavljena Ekipa',
+            style: Theme.of(context).textTheme.headline1?.copyWith(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+          ),
+          Spacers.h16,
+          Spacers.h8,
+          ...children
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
       extendBodyBehindAppBar: true,
-      body: Column(
-        children: [
-          BottomButton(
-            onPressed: () {},
-            text: 'Prijavi se u postojeću igru',
-          )
-        ],
-      ),
+      body: room == null
+          ? const LoadingIndicator()
+          : StreamBuilder(
+              stream: roomService.joinRoom(room!.id),
+              builder: (BuildContext context, AsyncSnapshot<Room> snapshot) {
+                if (!snapshot.hasData) {
+                  return const LoadingIndicator();
+                }
+
+                return Column(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 200,
+                            width: 200,
+                            padding: AppPadding.a24,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(24),
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: const Offset(0, 10),
+                                    blurRadius: 18,
+                                    spreadRadius: -8,
+                                    color: Theme.of(context).primaryColor.withOpacity(.2))
+                              ],
+                            ),
+                            child: QrImage(
+                              data: snapshot.data!.id,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).backgroundColor,
+                        borderRadius:
+                            const BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          createPlayersList(room!.players),
+                          Material(
+                            child: BottomButton(
+                              onPressed: () {},
+                              isDark: true,
+                              text: 'Kreni s igrom',
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
     );
   }
 }
